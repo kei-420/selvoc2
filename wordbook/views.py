e1c0c5b91f491063e25ebbe7cd0c4d8cc7d4c9cf
kei-420 <mail.yoshizumi@gmail.com>
2018-10-25 18:35:00 +0900

    Updated

--- wordbook/views.py
+++ wordbook/views.py
@@ -13,10 +13,6 @@ class Home(LoginRequiredMixin, ListView):
     paginate_by = 10
     template_name = 'wordbook/home.html'
 
-    # def get_queryset(self):
-    #     self.request.user = get_object_or_404(UsersManager, name=self.kwargs['username'])
-    #     return UsersManager.objects.filter(username=self.request.user)
-
 
 class WordAddView(LoginRequiredMixin, FormView):
     model = UserWordbook
@@ -33,31 +29,11 @@ class WordAddView(LoginRequiredMixin, FormView):
         form = WordAddForm(request.POST)
         if not form.is_valid():
             return render(request, 'wordbook/create_word.html', {'form': form})
-        # login_user = UserWordbook.objects.get(user=request.POST.user)
-        # word_data = form.cleaned_data.get('word')
-        # word_data = form.save(commit=False)
-        # queryset = Wordbook.objects.all()
-        # inner_join = Wordbook.objects.select_related('word')
-        #
-        # context = {
-        #         'word_data': word_data,
-        #         'vocabulary_list': inner_join,
-        #         'queryset': queryset,
-        # }
-        #
-        # word_data.save()
-        # return render(request, 'wordbook/home.html', context)
-            # get_user = UserWordbook.objects.get(user=login_user)
-        # login_user = UsersManager.objects.first()
+
         word_data = form.cleaned_data.get('word')
         exiting_word = Wordbook.objects.filter(vocabulary=word_data)
-        # adding_word.word =
-        # adding_word = UsersManager.objects.filter().add(word_data)
-
-        # inner_join = Wordbook.objects.filter(userwordbook__user_id=login_user.user_id)
-        # inner_join.word = word_data
+        # adding_word = Wordbook.objects.filter(userwordbook__word=exiting_word)
         context = {
-            'word_data': word_data,
             'vocabulary_list': exiting_word,
         }
         return render(request, 'wordbook/home.html', context)
